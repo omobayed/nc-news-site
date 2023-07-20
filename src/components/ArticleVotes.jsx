b980c47d748ab6b0ce8c342dd4e329bb64dd58fd
Ola Mobayed <ola.mobayed@gmail.com>
2023-07-20 11:00:05 +0100

    pr comments

--- src/components/ArticleVotes.jsx
+++ src/components/ArticleVotes.jsx
@@ -5,47 +5,31 @@ const ArticleVotes = ({ article_id, votes_count }) => {
     const [userVotes, setUserVotes] = useState(0);
     const [isError, setIsError] = useState(false)
 
-    const handleLikeClick = (event) => {
+    const handleClick = (event, increase) => {
         setIsError(false)
         event.preventDefault();
 
         setUserVotes((currentUserVote) => {
-            return currentUserVote + 1;
+            return currentUserVote + increase;
         })
 
-        updateArticleVotes(article_id, 1)
+        updateArticleVotes(article_id, increase)
             .catch((error) => {
                 setIsError(true)
                 setUserVotes((currentUserVote) => {
-                    return currentUserVote - 1;
+                    return currentUserVote - increase;
                 })
             })
 
-    }
-
-    const handleUnlikeClick = (event) => {
-        setIsError(false)
-        event.preventDefault();
-
-        setUserVotes((currentUserVote) => {
-            return currentUserVote - 1;
-        })
-        updateArticleVotes(article_id, -1)
-            .catch((error) => {
-                setIsError(true)
-                setUserVotes((currentUserVote) => {
-                    return currentUserVote + 1;
-                })
-            })
     }
 
     return (
-        <div>
-            <button aria-label="vote for article" onClick={handleLikeClick} disabled={userVotes > 0}>&#128077;</button>
-            <button aria-label="vote for article" onClick={handleUnlikeClick} disabled={userVotes < 0}>&#128078;</button>
+       <section>
+            <button aria-label="vote for article" onClick={e => handleClick(e, 1)} disabled={userVotes > 0}>&#128077;</button>
+            <button aria-label="vote for article" onClick={e => handleClick(e, -1)} disabled={userVotes < 0}>&#128078;</button>
             {isError ? <span style={{color:'red'}}>something went wrong! </span> : null}
             votes: {votes_count + userVotes}
-        </div>
+        </section>
     );
 }
 
